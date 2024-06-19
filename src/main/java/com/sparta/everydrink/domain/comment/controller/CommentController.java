d98fb5d662b69fdb4613a2fd52f2d51c41922043
Jaeseok Lee <wotjr4017@gmail.com>
2024-06-19 19:57:31 +0900

    Update : 댓글 작성 Controller 추가

--- src/main/java/com/sparta/everydrink/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/everydrink/domain/comment/controller/CommentController.java
@@ -1,4 +1,25 @@
 package com.sparta.everydrink.domain.comment.controller;
 
+import com.sparta.everydrink.domain.comment.dto.CommentRequestDto;
+import com.sparta.everydrink.domain.comment.dto.CommentResponseDto;
+import com.sparta.everydrink.domain.comment.service.CommentService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/post/{postId}/comment")
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<CommentResponseDto> create(@PathVariable(value = "postId") Long postId,
+                                                     @RequestBody CommentRequestDto requestDto,
+                                                     HttpServletRequest request, HttpServletResponse response) {
+        return commentService.createComment(postId, requestDto, request, response);
+    }
 }
