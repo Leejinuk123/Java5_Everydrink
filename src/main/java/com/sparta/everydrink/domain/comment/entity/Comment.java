db7139da2d6d68cc4273f3f636f7a115e5d91691
Jaeseok Lee <wotjr4017@gmail.com>
2024-06-19 20:03:00 +0900

    Add : Comment Entity 생성

--- src/main/java/com/sparta/everydrink/domain/comment/entity/Comment.java
+++ src/main/java/com/sparta/everydrink/domain/comment/entity/Comment.java
@@ -1,4 +1,44 @@
 package com.sparta.everydrink.domain.comment.entity;
 
+import com.sparta.everydrink.domain.post.entity.Post;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "comments")
+@NoArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public Comment(Long id, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
