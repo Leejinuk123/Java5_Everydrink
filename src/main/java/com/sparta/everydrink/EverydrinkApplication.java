1b8902382334b560a128c0b669c9646c93ed6461
leejinuk <leejinouk123@kakao.com>
2024-06-19 14:37:08 +0900

    ADD: Project init

---
+++ src/main/java/com/sparta/everydrink/EverydrinkApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.everydrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverydrinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverydrinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/everydrink/EverydrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.everydrink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydrinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
