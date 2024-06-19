275ecdf7a153f18ee475c71a2c48a507a7557ce2
leejinuk <leejinouk123@kakao.com>
2024-06-19 18:01:53 +0900

    Update: UserRepository 에 username 으로 user 데이터 가져오는 쿼리메소드 생성

--- src/main/java/com/sparta/everydrink/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/everydrink/domain/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.sparta.everydrink.domain.user.repository;
 
-public interface UserRepository {
+import com.sparta.everydrink.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
