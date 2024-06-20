65f7d230bd28099deea3acf8e5e48586454a2c55
jaewonchoi1234 <qwaszx9743@naver.com>
2024-06-20 12:53:23 +0900

    🎉 ADD : 회원가입 기능 구현
    추후에 예외처리 필요

--- src/main/java/com/sparta/everydrink/domain/user/entity/User.java
+++ src/main/java/com/sparta/everydrink/domain/user/entity/User.java
@@ -1,4 +1,69 @@
 package com.sparta.everydrink.domain.user.entity;
 
+import com.sparta.everydrink.domain.user.dto.UserSignupRequestDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "users")
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable =false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRoleEnum role;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatusEnum status;
+
+    private String refreshToken;
+
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modified_at;
+
+
+    public User(UserSignupRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.password = requestDto.getPassword();
+        this.nickname = requestDto.getNickname();
+        this.role = requestDto.getRole();
+        this.status = UserStatusEnum.ACTIVE;
+    }
 }
