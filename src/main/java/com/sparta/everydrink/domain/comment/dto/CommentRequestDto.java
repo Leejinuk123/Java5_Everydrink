0e6419203b5301b8783b2f34ba429dbf61bcfdd3
Jaeseok Lee <wotjr4017@gmail.com>
2024-06-20 13:22:41 +0900

    Add : Comment Dto 생성

--- src/main/java/com/sparta/everydrink/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/sparta/everydrink/domain/comment/dto/CommentRequestDto.java
@@ -1,4 +1,12 @@
 package com.sparta.everydrink.domain.comment.dto;
 
+import lombok.Getter;
+
+@Getter
 public class CommentRequestDto {
+    private String content;
+
+    public CommentRequestDto(String content) {
+        this.content = content;
+    }
 }
--- src/main/java/com/sparta/everydrink/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/everydrink/domain/comment/dto/CommentResponseDto.java
@@ -1,4 +1,14 @@
 package com.sparta.everydrink.domain.comment.dto;
 
+import com.sparta.everydrink.domain.comment.entity.Comment;
+
 public class CommentResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.content = comment.getContent();
+    }
 }
