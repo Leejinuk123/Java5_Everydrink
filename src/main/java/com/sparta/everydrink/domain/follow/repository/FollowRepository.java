0434732c5848945f38daee675253be582969ee39
Jaeseok Lee <wotjr4017@gmail.com>
2024-06-21 20:38:51 +0900

    FIX : FollowRepository 타입 수정

--- src/main/java/com/sparta/everydrink/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/sparta/everydrink/domain/follow/repository/FollowRepository.java
@@ -4,10 +4,11 @@ import com.sparta.everydrink.domain.follow.entity.Follow;
 import com.sparta.everydrink.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     Optional<Follow> findByFromUserAndToUser(User fromUser, User toUser);
 
-    Optional<Follow> findByFromUser(User currentUser);
+    List<Follow> findByFromUser(User currentUser);
 }
